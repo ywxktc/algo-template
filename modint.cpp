f56b71775a1da0ed9b8b4f7db53aa636d8c9fb4b
ywxktc <1453018126@qq.com>
2024-09-16 19:56:22 +0800

    modint

--- modint.cpp
+++ modint.cpp
@@ -1,16 +1,20 @@
 template<int m>
-class modint {
+struct modint {
 private:
   int _v = 0;
 public:
   modint(): _v(0) {}
-  modint(int v) { 
-    _v = v % m;
+
+  template<typename T>
+  modint(T v) {
+    _v = (int) (v % m);
     if (_v < 0) {
       _v += m;
     }
   }
-  int val() { return _v; }
+
+  int val() const { return _v; }
+
   modint& operator++() {
     _v++;
     if (_v >= m) {
@@ -18,6 +22,7 @@ public:
     }
     return *this;
   }
+
   modint& operator--() {
     _v--;
     if (_v < 0) {
@@ -25,16 +30,19 @@ public:
     }
     return *this;
   }
+
   modint operator++(int) {
     modint res = *this;
     ++*this;
     return res;
   }
+
   modint operator--(int) {
     modint res = *this;
     --*this;
     return res;
   }
+
   modint& operator+=(const modint& rhs) {
     _v += rhs._v;
     if (_v >= m) {
@@ -42,6 +50,7 @@ public:
     }
     return *this;
   }
+
   modint& operator-=(const modint& rhs) {
     _v -= rhs._v;
     if (_v < 0) {
@@ -49,16 +58,22 @@ public:
     }
     return *this;
   }
+
   modint& operator*=(const modint& rhs) {
     long long z = _v;
     z *= rhs._v;
     _v = (int) (z % m);
     return *this;
   }
+
   modint& operator/=(const modint& rhs) { return *this = *this * rhs.inv(); }
+
   modint operator+() const { return *this; }
-  modint operator-() const {return modint() - *this;}
-  modint pow(long long n) const {
+
+  modint operator-() const { return modint() - *this; }
+
+  template<typename T>
+  modint pow(T n) const {
     assert(n >= 0);
     modint x = *this, r = 1;
     while(n) {
@@ -68,26 +83,46 @@ public:
     }
     return r;
   }
+
+  modint inv() const { return exgcd(_v, m); }
+
   modint exgcd(int a, int b) const {
     int x = 0, y = 1;
     while (a != 0) {
       int t = b / a;
-      b -= t * a; swap(a, b);
-      x -= t * y; swap(x, y);
+      b -= t * a;
+      swap(a, b);
+      x -= t * y;
+      swap(x, y);
     }
     assert(b == 1);
     return modint(x);
   }
-  modint inv() const { return exgcd(_v, m); }
+
   friend modint operator+(const modint& lhs, const modint& rhs) { return modint(lhs) += rhs; }
+
   friend modint operator-(const modint& lhs, const modint& rhs) { return modint(lhs) -= rhs; }
+
   friend modint operator*(const modint& lhs, const modint& rhs) { return modint(lhs) *= rhs; }
+
   friend modint operator/(const modint& lhs, const modint& rhs) { return modint(lhs) /= rhs; }
+
   friend bool operator==(const modint& lhs, const modint& rhs) { return lhs._v == rhs._v; }
+
   friend bool operator!=(const modint& lhs, const modint& rhs) { return lhs._v != rhs._v; }
-  friend ostream& operator<<(ostream& os, const modint& rhs) { os << rhs._v; return os; }
-  friend istream& operator>>(istream& is, modint& rhs) { int v; is >> v; rhs = modint(v); return is; }
-};
+
+  friend istream& operator>>(istream& in, modint<m>& x) {
+    long long v;
+    cin >> v;
+    x = modint(v);
+    return in;
+  }
+  
+  friend ostream& operator<<(ostream& out, const modint<m>& x) {
+    out << x._v;
+    return out;
+  }
+}; // modint
 
 const int mod = ;
 using mint = modint<mod>;
