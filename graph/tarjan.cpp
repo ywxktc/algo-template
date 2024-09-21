9cb38fe7406117b2b18887a71dcb208502431b9d
ywxktc <1453018126@qq.com>
2024-09-21 15:47:07 +0800

    tarjan

--- graph/tarjan.cpp
+++ graph/tarjan.cpp
@@ -1,73 +1,63 @@
 struct Tarjan {
   int n = 0; // 顶点数量
-  vector<vector<int>> g; // 邻接图
-  vector<int> id; // 顶点编号
-  vector<int> low; // low-link值
-  vector<int> scc_id; // 顶点强连通分量编号
-  vector<vector<int>> scc; // 强联通分量
-  vector<vector<int>> scc_g; // 压缩后的强连通分量
+  vector<vector<int>> adj; // 邻接表
+  vector<int> dfn, low, comp; // 时间戳, low-link 值, 强连通分量编号
+  vector<vector<int>> scc, dag; // 强连通分量, 压缩图
 
-  Tarjan(int _n) : n(_n), g(_n), id(_n, -1), low(_n), scc_id(_n, -1) {}
+  Tarjan(int _n) : n(_n), adj(_n), dfn(_n, -1), low(_n), comp(_n, -1) {}
 
   void AddEdge(int u, int v) {
-    assert(0 <= u && u < n && 0 <= v && v < n);
-    g[u].push_back(v);
+    adj[u].push_back(v);
   }
 
   const vector<vector<int>>& FindSCC() {
-    int timer = 0;
+    int t = 0;
     stack<int> stk;
-    vector<bool> instk(n);
+    vector<bool> in_stk(n);
     function<void(int)> Dfs = [&](int u) {
-      id[u] = low[u] = timer++;
+      dfn[u] = low[u] = t++;
       stk.push(u);
-      instk[u] = true;
-
-      for (int v : g[u]) {
-        if (id[v] == -1) {
+      in_stk[u] = true;
+      for (int v : adj[u]) {
+        if (dfn[v] == -1) {
           Dfs(v);
           low[u] = min(low[u], low[v]);
-        } else if (instk[v]) {
-          low[u] = min(low[u], id[v]);
+        } else if (in_stk[v]) {
+          low[u] = min(low[u], dfn[v]);
         }
       }
-
-      if (id[u] == low[u]) {
+      if (dfn[u] == low[u]) {
         scc.push_back({});
         while (true) {
           int v = stk.top();
           stk.pop();
-          instk[v] = false;
-          scc_id[v] = scc.size() - 1;
+          in_stk[v] = false;
+          comp[v] = scc.size() - 1;
           scc.back().push_back(v);
           if (v == u) break;
         }
       }
     };
 
-    for (int u = 0; u < n; u++) {
-      if (id[u] == -1) {
-        Dfs(u);
-      }
+    for (int u = 0; u < n; ++u) {
+      if (dfn[u] == -1) Dfs(u);
     }
-
     return scc;
   }
 
   const vector<vector<int>>& Compress() {
-    scc_g.resize(scc.size());
-    for (int u = 0; u < n; u++) {
-      for (int v : g[u]) {
-        if (scc_id[u] != scc_id[v]) {
-          scc_g[scc_id[u]].push_back(scc_id[v]);
+    dag.resize(scc.size());
+    for (int u = 0; u < n; ++u) {
+      for (int v : adj[u]) {
+        if (comp[u] != comp[v]) {
+          dag[comp[u]].push_back(comp[v]);
         }
       }
     }
-    for (auto &adj : scc_g) {
-      sort(adj.begin(), adj.end());
-      adj.erase(unique(adj.begin(), adj.end()), adj.end());
-      adj.shrink_to_fit();
+    for (auto &v : dag) {
+      sort(v.begin(), v.end());
+      v.erase(unique(v.begin(), v.end()), v.end());
     }
-    return scc_g;
+    return dag;
   }
 };
