e092a9a9783ae3badb92a2badf26b969d8797012
ywxktc <1453018126@qq.com>
2025-07-06 14:20:27 +0800

    debug

--- debug.h
+++ debug.h
@@ -1,116 +1,135 @@
 #ifndef DEBUG_TEMPLATE_CPP
 #define DEBUG_TEMPLATE_CPP
-#include <bits/stdc++.h>
-// #define cerr cout
-namespace __DEBUG_UTIL__
-{
-    using namespace std;
-    template <typename T>
-    concept is_iterable = requires(T &&x) { begin(x); } &&
-                          !is_same_v<remove_cvref_t<T>, string>;
-    void print(const char *x) { cerr << x; }
-    void print(char x) { cerr << "\'" << x << "\'"; }
-    void print(bool x) { cerr << (x ? "T" : "F"); }
-    void print(string x) { cerr << "\"" << x << "\""; }
-    void print(vector<bool> &v)
-    { /* Overloaded this because stl optimizes vector<bool> by using
-         _Bit_reference instead of bool to conserve space. */
-        int f = 0;
-        cerr << '{';
-        for (auto &&i : v)
-            cerr << (f++ ? "," : "") << (i ? "T" : "F");
-        cerr << "}";
-    }
-    template <typename T>
-    void print(T &&x)
-    {
-        if constexpr (is_iterable<T>)
-            if (size(x) && is_iterable<decltype(*(begin(x)))>)
-            { /* Iterable inside Iterable */
-                int f = 0;
-                cerr << "\n~~~~~\n";
-                for (auto &&i : x)
-                {
-                    cerr << setw(2) << left << f++, print(i), cerr << "\n";
-                }
-                cerr << "~~~~~\n";
-            }
-            else
-            { /* Normal Iterable */
-                int f = 0;
-                cerr << "{";
-                for (auto &&i : x)
-                    cerr << (f++ ? "," : ""), print(i);
-                cerr << "}";
-            }
-        else if constexpr (requires { x.pop(); }) /* Stacks, Priority Queues, Queues */
-        {
-            auto temp = x;
-            int f = 0;
-            cerr << "{";
-            if constexpr (requires { x.top(); })
-                while (!temp.empty())
-                    cerr << (f++ ? "," : ""), print(temp.top()), temp.pop();
-            else
-                while (!temp.empty())
-                    cerr << (f++ ? "," : ""), print(temp.front()), temp.pop();
-            cerr << "}";
-        }
-        else if constexpr (requires { x.first; x.second; }) /* Pair */
-        {
-            cerr << '(', print(x.first), cerr << ',', print(x.second), cerr << ')';
-        }
-        else if constexpr (requires { get<0>(x); }) /* Tuple */
-        {
-            int f = 0;
-            cerr << '(', apply([&f](auto... args)
-                               { ((cerr << (f++ ? "," : ""), print(args)), ...); },
-                               x);
-            cerr << ')';
-        }
-        else
-            cerr << x;
-    }
-    template <typename T, typename... V>
-    void printer(const char *names, T &&head, V &&...tail)
-    {
-        int i = 0;
-        for (size_t bracket = 0; names[i] != '\0' and (names[i] != ',' or bracket != 0); i++)
-            if (names[i] == '(' or names[i] == '<' or names[i] == '{')
-                bracket++;
-            else if (names[i] == ')' or names[i] == '>' or names[i] == '}')
-                bracket--;
-        cerr.write(names, i) << " = ";
-        print(head);
-        if constexpr (sizeof...(tail))
-            cerr << " ||", printer(names + i + 1, tail...);
-        else
-            cerr << "]\n";
-    }
-    template <typename T, typename... V>
-    void printerArr(const char *names, T arr[], size_t N, V... tail)
-    {
-        size_t i = 0;
-        for (; names[i] and names[i] != ','; i++)
-            cerr << names[i];
-        for (i++; names[i] and names[i] != ','; i++)
-            ;
-        cerr << " = {";
-        for (size_t ind = 0; ind < N; ind++)
-            cerr << (ind ? "," : ""), print(arr[ind]);
-        cerr << "}";
-        if constexpr (sizeof...(tail))
-            cerr << " ||", printerArr(names + i + 1, tail...);
-        else
-            cerr << "]\n";
-    }
 
+#include <bits/stdc++.h>
+
+namespace debug_util {
+
+using std::cerr;
+using std::endl;
+using std::string;
+
+template <typename T>
+concept Iterable = requires(T &&x) {
+  std::begin(x);
+} && !std::is_same_v<std::remove_cvref_t<T>, string>;
+
+inline void Print(const char *x) { cerr << x; }
+
+inline void Print(char x) { cerr << '\'' << x << '\''; }
+
+inline void Print(bool x) { cerr << (x ? "true" : "false"); }
+
+inline void Print(const string &x) { cerr << '"' << x << '"'; }
+
+inline void Print(const std::vector<bool> &v) {
+  cerr << '{';
+  bool first = true;
+  for (bool b : v) {
+    if (!first)
+      cerr << ',';
+    cerr << (b ? "true" : "false");
+    first = false;
+  }
+  cerr << '}';
 }
+
+template <typename T>
+void Print(const T &x);
+
+template <typename A, typename B>
+inline void Print(const std::pair<A, B> &p) {
+  cerr << '(';
+  Print(p.first);
+  cerr << ',';
+  Print(p.second);
+  cerr << ')';
+}
+
+template <typename... Args>
+inline void Print(const std::tuple<Args...> &t) {
+  cerr << '(';
+  std::apply(
+      [](const auto &...args) {
+        size_t n = 0;
+        (..., (n++ > 0 ? (cerr << ',', Print(args)) : Print(args)));
+      },
+      t);
+  cerr << ')';
+}
+
+template <typename T>
+void Print(const T &x) {
+  if constexpr (Iterable<T>) {
+    cerr << '{';
+    bool first = true;
+    for (const auto &item : x) {
+      if (!first)
+        cerr << ',';
+      Print(item);
+      first = false;
+    }
+    cerr << '}';
+  } else if constexpr (requires { x.top(); x.empty(); }) { // stack, priority_queue
+    auto temp = x;
+    cerr << '{';
+    bool first = true;
+    while (!temp.empty()) {
+      if (!first)
+        cerr << ',';
+      Print(temp.top());
+      temp.pop();
+      first = false;
+    }
+    cerr << '}';
+  } else if constexpr (requires { x.front(); x.empty(); }) { // queue
+    auto temp = x;
+    cerr << '{';
+    bool first = true;
+    while (!temp.empty()) {
+      if (!first)
+        cerr << ',';
+      Print(temp.front());
+      temp.pop();
+      first = false;
+    }
+    cerr << '}';
+  } else {
+    cerr << x;
+  }
+}
+
+template <typename T, typename... Rest>
+void DebugPrinter(const char *names, T &&head, Rest &&...tail) {
+  while (*names == ' ' || *names == ',')
+    ++names;
+  const char *end = names;
+  int bracket = 0;
+  while (*end && (*end != ',' || bracket > 0)) {
+    if (*end == '(' || *end == '<' || *end == '{')
+      ++bracket;
+    if (*end == ')' || *end == '>' || *end == '}')
+      --bracket;
+    ++end;
+  }
+  cerr.write(names, end - names) << " = ";
+  Print(head);
+  if constexpr (sizeof...(tail)) {
+    cerr << " | ";
+    DebugPrinter(end, std::forward<Rest>(tail)...);
+  } else {
+    cerr << endl;
+  }
+}
+
+} // namespace debug_util
+
 #ifdef LOCAL
-#define debug(...) std::cerr << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
-#define debugArr(...) std::cerr << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+#define debug(...)               \
+  std::cerr << __LINE__ << ": "; \
+  debug_util::DebugPrinter(#__VA_ARGS__, __VA_ARGS__)
 #else
 #define debug(...)
-#define debugArr(...)
 #endif
-#endif
+
+#endif // DEBUG_TEMPLATE_CPP
